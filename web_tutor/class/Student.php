05d57e1c56dda21ae33d09c65a45568cb77a7ec0
unknown <riaz.i3216@gmail.com>
2019-12-01 17:58:58 +0600

    in 4

--- web_tutor/class/Student.php
+++ web_tutor/class/Student.php
@@ -4,4 +4,13 @@
 class Student
 {
 
+    //declare variables
+
+    public $name ;
+    public $email;
+    public $mobile;
+
+    private $conn;
+    private $table_name; 
+
 }
--- web_tutor/config/Database.php
+++ web_tutor/config/Database.php
@@ -3,5 +3,33 @@
 
 class Database
 {
+    //variable declaration
+    private $hostname;
+    private $dbname;
+    private $username;
+    private $password;
+
+    private $conn;
+
+    public function connect(){
+
+        //variable initialization
+        $this->hostname = "localhost";
+        $this->dbname = "rest_web_tutor";
+        $this->username = "root";
+        $this->password = "";
+
+        $this->conn = new mysqli($this->hostname,$this->username,$this->password,$this->dbname);
+
+        if($this->conn->connect_errno){
+
+            print_r($this->conn->connect_error);
+            exit();
+        }else{
+
+            return $this->conn;
+        }
+
+    }
 
 }
